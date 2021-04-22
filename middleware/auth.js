8b708df70d74b6efa52e04ddf1bdc24078193396
alivanov <ivanov@intspirit.com>
2021-04-22 15:22:27 +0300

    jwt auth simple

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../db/config.json")[process.env.NODE_ENV];
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token not provided!" });
+  }
+
+  const token = authHeader.replace("Bearer ", "");
+
+  try {
+    jwt.verify(tokeb, jwtSecret);
+  } catch (e) {
+    if (e instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ message: "Invalid token!" });
+    }
+  }
+
+  next();
+};
